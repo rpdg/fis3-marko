c80fb0da256afc0522632d42edae22a0f2ae201f
Lyu Pengfei <pf_lyu@qq.com>
2018-12-26 11:00:36 +0800

    update

---
+++ 1.marko.js
@@ -0,0 +1,54 @@
+// Compiled using marko@4.14.7 - DO NOT EDIT
+"use strict";
+
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/demo$1.0.0/1.marko",
+    components_helpers = require("marko/src/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_loadTag = marko_helpers.t,
+    component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
+    marko_escapeXml = marko_helpers.x,
+    init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  const orgName = 'orgName' ;
+
+  out.w("<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><meta http-equiv=\"cache-control\" content=\"max-age=0\"><link rel=\"stylesheet\" href=\"/page/style.css\"></head><body>");
+
+  component_globals_tag({}, out);
+
+  out.w("<div class=\"shower\">" +
+    marko_escapeXml(input.title) +
+    marko_escapeXml(orgName));
+
+  JSON.stringify(input.object2);
+
+  out.w("</div>");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "7");
+
+  out.w("</body></html>");
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
+
+marko_template.meta = {
+    id: "/demo$1.0.0/1.marko",
+    tags: [
+      "marko/src/components/taglib/component-globals-tag",
+      "marko/src/components/taglib/init-components-tag",
+      "marko/src/taglibs/async/await-reorderer-tag"
+    ]
+  };
